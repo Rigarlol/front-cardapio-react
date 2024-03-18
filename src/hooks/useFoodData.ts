36ec8a8a8ac0087b8fe5f388bad9c8468bb15cd7
Rigarlol <xrigarxd@gmail.com>
2024-03-18 08:56:59 -0300

    ddd

--- src/hooks/useFoodData.ts
+++ src/hooks/useFoodData.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios"
 import { FoodData } from '../interface/FoodDate';
 
-const API_URL = 'http://ec2-18-228-228-147.sa-east-1.compute.amazonaws.com:8080';
+const API_URL = 'https://ec2-18-228-228-147.sa-east-1.compute.amazonaws.com:8080';
 
 const fetchData = async (): AxiosPromise<FoodData[]> => {
     const response = axios.get(API_URL + '/food');

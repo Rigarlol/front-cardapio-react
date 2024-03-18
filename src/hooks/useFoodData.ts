512c972a102dbbcd1879ca5227c0432e1c14ea5f
Rigarlol <xrigarxd@gmail.com>
2024-03-18 08:33:05 -0300

    primeiro commit

---
+++ src/hooks/useFoodData.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosPromise } from "axios"
+import { FoodData } from '../interface/FoodDate';
+
+const API_URL = 'http://ec2-18-228-228-147.sa-east-1.compute.amazonaws.com:8080';
+
+const fetchData = async (): AxiosPromise<FoodData[]> => {
+    const response = axios.get(API_URL + '/food');
+    return response;
+}
+
+export function useFoodData(){
+    const query = useQuery({
+        queryFn: fetchData,
+        queryKey: ['food-data'],
+        retry: 2
+    })
+
+    return {
+        ...query,
+        data: query.data?.data
+    }
+}
---
+++ src/hooks/useFoodDataMutate.ts
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosPromise } from "axios"
+import { FoodData } from '../interface/FoodDate';
+
+const API_URL = 'http://ec2-18-228-228-147.sa-east-1.compute.amazonaws.com:8080';
+
+const postData = async (data: FoodData): AxiosPromise<any> => {
+    const response = axios.post(API_URL + '/food', data);
+    return response;
+}
+
+export function useFoodDataMutate(){
+    const queryClient = useQueryClient();
+    const mutate = useMutation({
+        mutationFn: postData,
+        retry: 2,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['food-data'])
+        }
+    })
+
+    return mutate;
+}

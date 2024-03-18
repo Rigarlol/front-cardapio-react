21792477d9b908891f5593c3246272dc3dfa08a7
Rigarlol <xrigarxd@gmail.com>
2024-03-18 09:36:11 -0300

    correção

--- src/hooks/useFoodDataMutate.ts
+++ src/hooks/useFoodDataMutate.ts
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios"
 import { FoodData } from '../interface/FoodDate';
 
-const API_URL = 'https://ec2-18-228-228-147.sa-east-1.compute.amazonaws.com:8080';
+const API_URL = 'http://ec2-18-228-228-147.sa-east-1.compute.amazonaws.com:8080';
 
 const postData = async (data: FoodData): AxiosPromise<any> => {
     const response = axios.post(API_URL + '/food', data);
